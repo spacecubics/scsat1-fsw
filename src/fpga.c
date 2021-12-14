1307e56ae93414527bb66d303ae2a5cd9ae0a4cf
Daisuke Sasaki <daisuke@spacecubics.com>
2021-12-14 20:32:43 +0900

    fpga: Bug fixes and refactorings for fpga configuration logic

--- src/fpga.c
+++ src/fpga.c
@@ -18,28 +18,23 @@ void fpga_init (void) {
 }
 
 void check_fpga_power (void) {
-        if (VDD_3V3) {
+        if (VDD_3V3)
                 switch_fpga_state (ST_FPGA_READY);
-        } else {
-                FPGA_PROGRAM_B_DIR = 1;
-                FPGA_INIT_B_DIR = 1;
-        }
 }
 
 int switch_fpga_state (fpga_st next_st) {
         if (fmd.state == ST_POWER_OFF) {
                 if (next_st == ST_FPGA_READY) {
-                        FPGA_INIT_B = 0;
+                        fmd.state = ST_FPGA_READY;
                         FPGA_INIT_B_DIR = 0;
                         FPGA_PROGRAM_B = 1;
                         FPGA_PROGRAM_B_DIR = 0;
                 }
         } else if (fmd.state == ST_FPGA_READY) {
                 if (next_st == ST_FPGA_CONFIG) {
-                        if (fmd.count == 0) {
-                                FPGA_INIT_B = 0;
-                                FPGA_INIT_B_DIR = 0;
-                        } else
+                        if (fmd.count == 0)
+                                FPGA_INIT_B_DIR = 1;
+                        else
                                 FPGA_PROGRAM_B = 1;
                         fmd.state = ST_FPGA_CONFIG;
                         fmd.count++;
