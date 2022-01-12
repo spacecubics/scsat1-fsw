4a1fcb5b4b1ad8f4d9a76c3f348308ab5b0a30fd
Yasushi SHOJI <yashi@spacecubics.com>
2022-01-12 12:36:08 +0900

    fpga: Remove typedef from enum
    typedef doesn't buy you anything in this case.  Using enum Explicitly
    tells users what it is.
    
    Signed-off-by: Yasushi SHOJI <yashi@spacecubics.com>

--- src/fpga.h
+++ src/fpga.h
@@ -7,16 +7,16 @@
  *
  */
 
-typedef enum {
+enum FpgaState{
 	FPGA_STATE_POWER_OFF,
 	FPGA_STATE_READY,
 	FPGA_STATE_CONFIG,
 	FPGA_STATE_ACTIVE,
 	FPGA_STATE_LAST,
-} fpga_st;
+};
 
 typedef struct s_fpga_management_data {
-        fpga_st state;
+        enum FpgaState state;
         unsigned config_ok: 1;
         int count;
         int time;
