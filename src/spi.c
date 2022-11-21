c10065f52d93c627a460f6d7daa7d4a493bda653
Yasushi SHOJI <yashi@spacecubics.com>
2022-11-21 20:21:12 +0900

    spi: Add spi_read8, read16, read32 accessors
    Introduce convenient accessors for SPI reads.
    
    Signed-off-by: Yasushi SHOJI <yashi@spacecubics.com>

--- src/spi.c
+++ src/spi.c
@@ -49,3 +49,61 @@ uint8_t spi_trans (uint8_t buf) {
         rdata = SSPBUF;
         return rdata;
 }
+
+uint8_t spi_read8(uint16_t addr)
+{
+        uint8_t buf0;
+        uint8_t buf1;
+        uint8_t ret;
+
+        buf0 = ((addr >> 8) & 0xf) | 0x30;
+        buf1 = addr & 0xff;
+
+        spi_get();
+        spi_trans(buf0);
+        spi_trans(buf1);
+        ret = spi_trans(0);
+        spi_release();
+
+        return ret;
+}
+
+uint16_t spi_read16(uint16_t addr)
+{
+        uint8_t buf0;
+        uint8_t buf1;
+        uint16_t ret;
+
+        buf0 = ((addr >> 8) & 0xf) | 0x30;
+        buf1 = addr & 0xff;
+
+        spi_get();
+        spi_trans(buf0);
+        spi_trans(buf1);
+        ret = (((uint16_t)spi_trans(0) & 0xff) << 0);
+        ret = (((uint16_t)spi_trans(0) & 0xff) << 8) | ret;
+        spi_release();
+
+        return ret;
+}
+
+uint32_t spi_read32(uint16_t addr)
+{
+        uint8_t buf0;
+        uint8_t buf1;
+        uint32_t ret;
+
+        buf0 = ((addr >> 8) & 0xf) | 0x30;
+        buf1 = addr & 0xff;
+
+        spi_get();
+        spi_trans(buf0);
+        spi_trans(buf1);
+        ret = (((uint32_t)spi_trans(0) & 0xff) <<  0);
+        ret = (((uint32_t)spi_trans(0) & 0xff) <<  8) | ret;
+        ret = (((uint32_t)spi_trans(0) & 0xff) << 16) | ret;
+        ret = (((uint32_t)spi_trans(0) & 0xff) << 24) | ret;
+        spi_release();
+
+        return ret;
+}
--- src/spi.h
+++ src/spi.h
@@ -15,3 +15,6 @@ void spi_init (void);
 void spi_get (void);
 void spi_release (void);
 uint8_t spi_trans (uint8_t buf);
+uint8_t spi_read8(uint16_t addr);
+uint16_t spi_read16(uint16_t addr);
+uint32_t spi_read32(uint16_t addr);
