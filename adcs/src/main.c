55bacab15042b12d000d19825030186b6500819c
Yasushi SHOJI <yashi@spacecubics.com>
2023-05-11 10:54:00 +0900

    initial
    Signed-off-by: Yasushi SHOJI <yashi@spacecubics.com>

---
+++ adcs/src/main.c
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2023 Space Cubics, LLC.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/kernel.h>
+
+int main(void)
+{
+	printk("Hello World! %s\n", CONFIG_BOARD);
+	return 0;
+}
