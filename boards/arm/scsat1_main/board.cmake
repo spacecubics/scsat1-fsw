be66ef4d309a2dc98b7681714884883e42c1053d
Takuya Sasaki <takuya.sasaki@spacecubics.com>
2023-07-28 15:14:29 +0900

    boards: Add new target board for SC-SAT1 MAIN
    So far, We use dts overlay based on SC-OBC Module A1 for SC-Sat1
    MAIN board.
    However, we decided to prepare the new dts for SC-at1 MAIN board.
    Later we will prepare for SC-Sat1 ADCS boards.
    
    Signed-off-by: Takuya Sasaki <takuya.sasaki@spacecubics.com>

---
+++ boards/arm/scsat1_main/board.cmake
@@ -0,0 +1,7 @@
+# SPDX-License-Identifier: Apache-2.0
+
+if(CONFIG_BOARD_SCSAT1_MAIN)
+  board_runner_args(openocd "--use-elf" "--config=${BOARD_DIR}/support/openocd-ftdi.cfg")
+
+  include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
+endif()
