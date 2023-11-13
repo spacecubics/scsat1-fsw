7d31b5c1b3f43b3080f132df97234595982fa410
Takuya Sasaki <takuya.sasaki@spacecubics.com>
2023-11-13 15:57:31 +0900

    hwtest/main,adcs: Add power control API
    To power on the other boards (ADCS,Payload) and some devices, it is
    necessary to write the System Register of the FPGA. This commit adds
     an API to control the ON/OFF state of each power line.
    
    Signed-off-by: Takuya Sasaki <takuya.sasaki@spacecubics.com>

---
+++ tests/hwtest/main/src/pwrctrl.c
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2023 Space Cubics,LLC
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/kernel.h>
+
+/* Registers */
+#define SCMAIN_SYSREG_BASE_ADDR (0x50000000)
+#define SCMAIN_PCR_OFFSET (0x0000)
+
+/* Power Control Register */
+#define SCMAIN_PCR_KEYCODE (0x5A5A << 16)
+
+void sc_main_power_enable(uint8_t target_bit)
+{
+	sys_set_bits(SCMAIN_SYSREG_BASE_ADDR + SCMAIN_PCR_OFFSET,
+				 SCMAIN_PCR_KEYCODE | target_bit);
+}
+
+void sc_main_power_disable(uint8_t target_bit)
+{
+	sys_clear_bits(SCMAIN_SYSREG_BASE_ADDR + SCMAIN_PCR_OFFSET,
+				 SCMAIN_PCR_KEYCODE | target_bit);
+}
---
+++ tests/hwtest/main/src/pwrctrl.h
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2023 Space Cubics, LLC.↲
+ *↲
+ * SPDX-License-Identifier: Apache-2.0↲
+ */
+
+#pragma once
+
+#include <stdint.h>
+
+#define DRV2_PWR     BIT(5)
+#define DRV1_PWR     BIT(4)
+#define DSTRX_IO_PWR BIT(3)
+#define PDU_O3_PWR   BIT(2)
+#define PDU_O2_PWR   BIT(1)
+#define PDU_O1_PWR   BIT(0)
+
+void sc_main_power_enable(uint8_t target_bit);
+void sc_main_power_disable(uint8_t target_bit);
