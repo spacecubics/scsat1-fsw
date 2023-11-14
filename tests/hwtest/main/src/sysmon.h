60edf3d0e931bd2e035e5f15730236481d76874e
Takuya Sasaki <takuya.sasaki@spacecubics.com>
2023-11-14 20:17:01 +0900

    hwtest/main: Add Board Health Monitor API
    This commit adds an API for retrieving temperature and CV values
    on OBC module.
    
    Signed-off-by: Takuya Sasaki <takuya.sasaki@spacecubics.com>

---
+++ tests/hwtest/main/src/sysmon.h
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2023 Space Cubics, LLC.↲
+ *↲
+ * SPDX-License-Identifier: Apache-2.0↲
+ */
+
+#pragma once
+
+#include <stdint.h>
+
+enum obc_temp_pos {
+	OBC_TEMP_1,
+	OBC_TEMP_2,
+	OBC_TEMP_3,
+};
+
+enum obc_cv_pos {
+	OBC_1V0_SHUNT,
+	OBC_1V0_BUS,
+	OBC_1V8_SHUNT,
+	OBC_1V8_BUS,
+	OBC_3V3_SHUNT,
+	OBC_3VS_BUS,
+	OBC_3V3_SYSA_SHUNT,
+	OBC_3VS_SYSA_BUS,
+	OBC_3V3_SYSB_SHUNT,
+	OBC_3VS_SYSB_BUS,
+	OBC_3V3_IO_SHUNT,
+	OBC_3VS_IO_BUS,
+};
+
+enum xadc_cv_pos {
+	OBC_XADC_VCCINT,
+	OBC_XADC_VCCAUX,
+	OBC_XADC_VCCBRAM,
+};
+
+int sc_main_bhm_enable(void);
+int sc_main_bhm_disable(void);
+int sc_main_bhm_get_obc_temp(enum obc_temp_pos pos, float *temp);
+int sc_main_bhm_get_xadc_temp(float *temp);
+int sc_main_bhm_get_obc_cv(enum obc_cv_pos pos, uint32_t *cv);
+int sc_main_bhm_get_xadc_cv(enum xadc_cv_pos pos, float *cv);
