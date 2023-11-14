891b89d48daa0caff8d11de6023ac5892df78615
Takuya Sasaki <takuya.sasaki@spacecubics.com>
2023-11-14 23:37:54 +0900

    hwtest/main: Add OBC Temperature and CV test
    This commit adds OBC Temperature and Current/Voltage test.
    
    Signed-off-by: Takuya Sasaki <takuya.sasaki@spacecubics.com>

--- tests/hwtest/main/src/cv_test.c
+++ tests/hwtest/main/src/cv_test.c
@@ -6,11 +6,104 @@
 
 #include <zephyr/kernel.h>
 #include "cv.h"
+#include "sysmon.h"
 
 #include <zephyr/logging/log.h>
 LOG_MODULE_REGISTER(cv_test);
 
-int cv_test(uint32_t *err_cnt)
+static int cv_obc_test(uint32_t *err_cnt)
+{
+	int ret = 0;
+	int all_ret = 0;
+	uint32_t cv;
+	enum obc_cv_pos obc_pos_list[] = {
+		OBC_1V0_SHUNT,
+		OBC_1V0_BUS,
+		OBC_1V8_SHUNT,
+		OBC_1V8_BUS,
+		OBC_3V3_SHUNT,
+		OBC_3VS_BUS,
+		OBC_3V3_SYSA_SHUNT,
+		OBC_3VS_SYSA_BUS,
+		OBC_3V3_SYSB_SHUNT,
+		OBC_3VS_SYSB_BUS,
+		OBC_3V3_IO_SHUNT,
+		OBC_3VS_IO_BUS,
+	};
+	const char obc_pos_name[][20] = {
+		"OBC_1V0 Shunt",
+		"OBC_1V0 Bus",
+		"OBC_1V8 Shunt",
+		"OBC_1V8 Bus",
+		"OBC_3V3 Shunt",
+		"OBC_3VS Bus",
+		"OBC_3V3_SYSA Shunt",
+		"OBC_3VS_SYSA Bus",
+		"OBC_3V3_SYSB Shunt",
+		"OBC_3VS_SYSB Bus",
+		"OBC_3V3_IO Shunt",
+		"OBC_3VS_IO Bus",
+	};
+	const char obc_unit_name[][5] = {
+		"[uv]",
+		"[mv]",
+		"[uv]",
+		"[mv]",
+		"[uv]",
+		"[mv]",
+		"[uv]",
+		"[mv]",
+		"[uv]",
+		"[mv]",
+		"[uv]",
+		"[mv]",
+	};
+
+	for (int i=0; i<ARRAY_SIZE(obc_pos_list); i++) {
+		ret = sc_main_bhm_get_obc_cv(obc_pos_list[i], &cv);
+		if (ret < 0) {
+			LOG_ERR("%s: Failed", obc_pos_name[i]);
+			(*err_cnt)++;
+			all_ret = -1;
+			continue;
+		}
+		LOG_INF("%s: %d %s", obc_pos_name[i], cv, obc_unit_name[i]);
+	}
+
+	return all_ret;
+}
+
+static int cv_xadc_test(uint32_t *err_cnt)
+{
+	int ret = 0;
+	int all_ret = 0;
+	float cv;
+	enum xadc_cv_pos xadc_pos_list[] = {
+		OBC_XADC_VCCINT,
+		OBC_XADC_VCCAUX,
+		OBC_XADC_VCCBRAM,
+	};
+	const char xadc_pos_name[][18] = {
+		"OBC_XADC VCCINT",
+		"OBC_XADC VCCAUX",
+		"OBC_XADC VCCBRAM",
+	};
+
+	for (int i=0; i<ARRAY_SIZE(xadc_pos_list); i++) {
+		ret = sc_main_bhm_get_xadc_cv(xadc_pos_list[i], &cv);
+		if (ret < 0) {
+			LOG_ERR("%s: Failed", xadc_pos_name[i]);
+			(*err_cnt)++;
+			all_ret = -1;
+			continue;
+		}
+		LOG_INF("%s: %.4f [v]", xadc_pos_name[i], cv);
+	}
+
+	return all_ret;
+}
+
+static int cv_ioboard_test(uint32_t *err_cnt)
 {
 	int ret = 0;
 	int all_ret = 0;
@@ -49,6 +142,29 @@ int cv_test(uint32_t *err_cnt)
 			continue;
 		}
 		LOG_INF("%s: %d %s", io_pos_name[i], cv, io_unit_name[i]);
+	}
+
+	return all_ret;
+}
+
+int cv_test(uint32_t *err_cnt)
+{
+	int ret = 0;
+	int all_ret = 0;
+
+	ret = cv_obc_test(err_cnt);
+	if (ret < 0) {
+		all_ret = -1;
+	}
+
+	ret = cv_xadc_test(err_cnt);
+	if (ret < 0) {
+		all_ret = -1;
+	}
+
+	ret = cv_ioboard_test(err_cnt);
+	if (ret < 0) {
+		all_ret = -1;
 	}
 
 	return all_ret;
--- tests/hwtest/main/src/temp_test.c
+++ tests/hwtest/main/src/temp_test.c
@@ -6,13 +6,61 @@
 
 #include <zephyr/sys/util.h>
 #include "temp.h"
+#include "sysmon.h"
 
 #include <zephyr/logging/log.h>
 LOG_MODULE_REGISTER(temp_test);
 
-int temp_test(uint32_t *err_cnt)
+static int temp_obc_test(uint32_t *err_cnt)
 {
-	int ret = 0;
+	int ret;
+	int all_ret = 0;
+	float temp;
+	int i;
+	enum obc_temp_pos obc_pos_list[] = {
+		OBC_TEMP_1,
+		OBC_TEMP_2,
+		OBC_TEMP_3,
+	};
+	const char obc_pos_name[][6] = {
+		"OBC 1",
+		"OBC 2",
+		"OBC 3",
+	};
+
+	for (i=0; i<ARRAY_SIZE(obc_pos_list); i++) {
+		ret = sc_main_bhm_get_obc_temp(obc_pos_list[i], &temp);
+		if (ret < 0) {
+			LOG_ERR("%s Temperature: Failed", obc_pos_name[i]);
+			(*err_cnt)++;
+			all_ret = -1;
+			continue;
+		}
+		LOG_INF("%s Temperature: %.4f [deg]", obc_pos_name[i], temp);
+	}
+
+	return all_ret;
+}
+
+static int temp_xadc_test(uint32_t *err_cnt)
+{
+	int ret;
+	float temp;
+
+	ret = sc_main_bhm_get_xadc_temp(&temp);
+	if (ret < 0) {
+		LOG_ERR("OBC XADC Temperature: Failed");
+		(*err_cnt)++;
+	} else {
+		LOG_INF("OBC XADC Temperature: %.4f [deg]", temp);
+	}
+
+	return ret;
+}
+
+static int temp_ioboard_test(uint32_t *err_cnt)
+{
+	int ret;
 	int all_ret = 0;
 	float temp;
 	enum io_temp_pos io_pos_list[] = {
@@ -41,6 +89,29 @@ int temp_test(uint32_t *err_cnt)
 			continue;
 		}
 		LOG_INF("%s Temperature: %.4f [deg]", io_pos_name[i], temp);
+	}
+
+	return all_ret;
+}
+
+int temp_test(uint32_t *err_cnt)
+{
+	int ret;
+	int all_ret = 0;
+
+	ret = temp_obc_test(err_cnt);
+	if (ret < 0) {
+		all_ret = -1;
+	}
+
+	ret = temp_xadc_test(err_cnt);
+	if (ret < 0) {
+		all_ret = -1;
+	}
+
+	ret = temp_ioboard_test(err_cnt);
+	if (ret < 0) {
+		all_ret = -1;
 	}
 
 	return all_ret;
