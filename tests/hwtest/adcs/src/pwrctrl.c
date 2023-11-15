6fda8f84cafb1a20547755e87581ebd2b825c672
Takuya Sasaki <takuya.sasaki@spacecubics.com>
2023-11-15 19:24:57 +0900

    hwtest/adcs: Add IMU Reset related API
    This commit adds IMU Reset related API.
    
    Signed-off-by: Takuya Sasaki <takuya.sasaki@spacecubics.com>

--- tests/hwtest/adcs/src/pwrctrl.c
+++ tests/hwtest/adcs/src/pwrctrl.c
@@ -8,10 +8,15 @@
 
 /* Registers */
 #define SCADCS_SYSREG_BASE_ADDR (0x50000000)
-#define SCADCS_PCR_OFFSET (0x0000)
+#define SCADCS_PCR_OFFSET  (0x0000)
+#define SCADCS_IRCR_OFFSET (0x0004)
 
 /* Power Control Register */
 #define SCADCS_PCR_KEYCODE (0x5A5A << 16)
+
+/* IMU Reset Control Register */
+#define SCADCS_IMU_RESET         (0U)
+#define SCADCS_IMU_RESET_RELEASE BIT(0)
 
 void sc_adcs_power_enable(uint8_t target_bit)
 {
@@ -23,4 +28,18 @@ void sc_adcs_power_disable(uint8_t target_bit)
 {
 	sys_clear_bits(SCADCS_SYSREG_BASE_ADDR + SCADCS_PCR_OFFSET,
 				 SCADCS_PCR_KEYCODE | target_bit);
+}
+
+void sc_adcs_imu_reset(void)
+{
+	sys_set_bits(SCADCS_SYSREG_BASE_ADDR + SCADCS_IRCR_OFFSET,
+				 SCADCS_IMU_RESET);
+	k_sleep(K_USEC(1));
+}
+
+void sc_adcs_imu_reset_release(void)
+{
+	sys_set_bits(SCADCS_SYSREG_BASE_ADDR + SCADCS_IRCR_OFFSET,
+				 SCADCS_IMU_RESET_RELEASE);
+	k_sleep(K_MSEC(300));
 }
