e9c65ccc8850bcfb7117a60d6bd83f9720981e32
Takuya Sasaki <takuya.sasaki@spacecubics.com>
2023-11-18 20:12:18 +0900

    hwtest/adcs: Add Reacton Wheel test
    This commit adds the Reaction Wheel test.
    
    Signed-off-by: Takuya Sasaki <takuya.sasaki@spacecubics.com>

---
+++ tests/hwtest/adcs/src/rw.h
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2023 Space Cubics, LLC.↲
+ *↲
+ * SPDX-License-Identifier: Apache-2.0↲
+ */
+
+#pragma once
+
+#include <stdint.h>
+#include "pwrctrl.h"
+
+#define RW_MAX_POTENTION  (0x7F)
+#define RW_HALF_POTENTION (0x40)
+
+enum rw_pos {
+	RW_POS_X = 0,
+	RW_POS_Y,
+	RW_POS_Z,
+};
+
+static const char rw_pos_name[][5] = {
+	"RW X",
+	"RW Y",
+	"RW Z",
+};
+
+int rw_start(enum rw_pos pos);
+void rw_stop(enum rw_pos pos);
+int rw_change_speed(enum rw_pos pos, uint16_t pot);
+void rw_start_measurment(enum rw_pos pos);
+void rw_stop_measurment(enum rw_pos pos);
+int32_t rw_get_count(enum rw_pos pos);
+void rw_print_cv(enum rw_pos pos);
---
+++ tests/hwtest/adcs/src/rw_test.c
@@ -0,0 +1,58 @@
+/*
+ * Copyright (c) 2023 Space Cubics, LLC.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/kernel.h>
+#include "rw.h"
+
+#include <zephyr/logging/log.h>
+LOG_MODULE_REGISTER(rw_test);
+
+static void print_rw_status(enum rw_pos pos, uint32_t sec)
+{
+	for (int j=0; j<5; j++) {
+		k_sleep(K_SECONDS(1));
+		rw_print_cv(pos);
+		LOG_INF("%s count: %d", rw_pos_name[pos], rw_get_count(pos));
+	}
+}
+
+int rw_test(uint32_t *err_cnt)
+{
+	int ret;
+	int all_ret = 0;
+	enum rw_pos pos_list[] = {
+		RW_POS_X,
+		RW_POS_Y,
+		RW_POS_Z,
+	};
+
+	for (int i=0; i<ARRAY_SIZE(pos_list); i++) {
+		ret = rw_start(pos_list[i]);
+		if (ret < 0) {
+			(*err_cnt)++;
+			all_ret = -1;
+			goto end;
+		}
+
+		print_rw_status(pos_list[i], 5);
+
+		LOG_INF("Change Potention: 0x%02x on %s",
+			    RW_HALF_POTENTION, rw_pos_name[pos_list[i]]);
+		ret = rw_change_speed(pos_list[i], RW_HALF_POTENTION);
+		if (ret < 0) {
+			(*err_cnt)++;
+			all_ret = -1;
+			goto stop;
+		}
+
+		print_rw_status(pos_list[i], 5);
+stop:
+		rw_stop(pos_list[i]);
+end:
+	}
+
+	return all_ret;
+}
