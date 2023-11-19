82dc03847123262ae03aff965126c753aa6b0590
Takuya Sasaki <takuya.sasaki@spacecubics.com>
2023-11-19 22:21:49 +0900

    hwtest/main: Add init command for MAIN Board
    This commit adds initialization commands to enable the power of
    each board before testing.
    
    Signed-off-by: Takuya Sasaki <takuya.sasaki@spacecubics.com>

---
+++ tests/hwtest/main/src/main_init.c
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c) 2023 Space Cubics, LLC.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/kernel.h>
+#include "pwrctrl.h"
+#include "sysmon.h"
+#include "csp.h"
+
+#include <zephyr/logging/log.h>
+LOG_MODULE_REGISTER(main_init);
+
+
+#include <stdint.h>
+
+#define DRV2_PWR     BIT(5)
+#define DRV1_PWR     BIT(4)
+#define DSTRX_IO_PWR BIT(3)
+#define PDU_O3_PWR   BIT(2)
+#define PDU_O2_PWR   BIT(1)
+#define PDU_O1_PWR   BIT(0)
+
+int main_init(uint32_t *err_cnt)
+{
+	int ret;
+	int all_ret = 0;
+
+	ret = sc_main_bhm_enable();
+	if (ret < 0) {
+		LOG_ERR("Failed to enable the BHM. (%d)", ret);
+		(*err_cnt)++;
+		all_ret = -1;
+	} else {
+		LOG_INF("Enable the BHM");
+	}
+
+	sc_main_power_enable(DRV2_PWR | DRV1_PWR);
+	LOG_INF("Power on the DRV1/DRV2");
+
+	k_sleep(K_SECONDS(1));
+
+	sc_main_power_enable(PDU_O1_PWR);
+	LOG_INF("Power on the ADCS Board");
+
+	k_sleep(K_SECONDS(1));
+
+	sc_main_power_enable(PDU_O2_PWR);
+	LOG_INF("Power on the Payload Board");
+
+	ret = csp_enable();
+	if (ret < 0) {
+		LOG_ERR("Failed to enable the CSP. (%d)", ret);
+		(*err_cnt)++;
+		all_ret = -1;
+	} else {
+		LOG_INF("Enable the CSP");
+	}
+
+	return all_ret;
+}
