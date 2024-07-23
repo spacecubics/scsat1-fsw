54bee600c0f85403dbfe0ffecd8e942d13cc7d22
Takuya Sasaki <takuya.sasaki@spacecubics.com>
2024-07-23 16:31:07 +0900

    bootloader: Replace to flash map API
    Previously, the copy of flight software code from configuration
    memory to HRMEM was done by directly call the SC QSPI IP core
    registers without using the Zephyr driver.
    This commit replaces by using the flash map API.
    
    Signed-off-by: Takuya Sasaki <takuya.sasaki@spacecubics.com>

--- bootloader/src/main.c
+++ bootloader/src/main.c
@@ -4,24 +4,56 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#include "qspi_norflash.h"
-
-#include <zephyr/logging/log.h>
-LOG_MODULE_REGISTER(bootloader);
+#include <string.h>
+#include <zephyr/kernel.h>
+#include <zephyr/storage/flash_map.h>
 
 #define SCOBCA1_FPGA_SYSREG_CODEMSEL (0x4F000000)
 #define SCOBCA1_FPGA_HRMEM_ADDR      (0x60000000)
 #define SYSREG_CODEMSEL_ENABLE_HRMEM (0x5a5a0000)
+#define QSPI_RX_FIFO_MAX_BYTE        (16U)
+
+static int copy_norflash_to_hrmem(uint8_t partition_id, mm_reg_t sram_mem_addr, uint32_t size)
+{
+	int ret;
+	const struct flash_area *flash = NULL;
+	uint8_t read_vals[QSPI_RX_FIFO_MAX_BYTE];
+	uint16_t loop_count;
+	off_t offset = 0;
+
+	ret = flash_area_open(partition_id, &flash);
+	if (ret < 0) {
+		goto end;
+	}
+
+	loop_count = size / QSPI_RX_FIFO_MAX_BYTE;
+	for (uint16_t i = 0; i < loop_count; i++) {
+
+		ret = flash_area_read(flash, offset, (void *)read_vals, QSPI_RX_FIFO_MAX_BYTE);
+		if (ret < 0) {
+			goto cleanup;
+		}
+
+		memcpy((void *)sram_mem_addr, read_vals, QSPI_RX_FIFO_MAX_BYTE);
+
+		offset += QSPI_RX_FIFO_MAX_BYTE;
+		sram_mem_addr += QSPI_RX_FIFO_MAX_BYTE;
+	}
+
+cleanup:
+	flash_area_close(flash);
+
+end:
+	return ret;
+}
 
 int main(void)
 {
 	int ret;
 
-	ret = qspi_norflash_copy_to_hrmem(QSPI_CFG_MEM0, CONFIG_SCOBC_A1_BOOT_CFG_MEM_ADDR,
-					  KB(CONFIG_SCOBC_A1_BOOT_CFG_COPY_SIZE_KB),
-					  SCOBCA1_FPGA_HRMEM_ADDR);
+	ret = copy_norflash_to_hrmem(FIXED_PARTITION_ID(fsw_partition), SCOBCA1_FPGA_HRMEM_ADDR,
+				     KB(CONFIG_SCOBC_A1_BOOT_CFG_COPY_SIZE_KB));
 	if (ret < 0) {
-		LOG_ERR("Failed to copy the Zephyr application from Config memory to HRMEM.");
 		goto end;
 	}
 
