855187cbaa3d1bf482796d9e6d88a8289cc9464f
Takuya Sasaki <takuya.sasaki@spacecubics.com>
2024-07-25 15:39:59 +0900

    bootloader: Select CFG bank same as FPGA configuration
    FPGA configures using the Config memory bank specified by TRCH, but
    after control is transferred to the FPGA, the Config Memory selects
    the default 0 bank. Therefore, the Bootloader needs to check the
    FPGA registers to determine which bank was used for configuration,
    and then load the FSW from the same bank and copy it to HRMEM.
    
    Signed-off-by: Takuya Sasaki <takuya.sasaki@spacecubics.com>

--- bootloader/src/main.c
+++ bootloader/src/main.c
@@ -9,10 +9,50 @@
 #include <zephyr/storage/flash_map.h>
 #include "version.h"
 
-#define SCOBCA1_FPGA_SYSREG_CODEMSEL (0x4F000000)
-#define SCOBCA1_FPGA_HRMEM_ADDR      (0x60000000)
-#define SYSREG_CODEMSEL_ENABLE_HRMEM (0x5a5a0000)
-#define QSPI_RX_FIFO_MAX_BYTE        (16U)
+#define SCOBCA1_FPGA_SYSREG_CODEMSEL  (0x4F000000)
+#define SCOBCA1_FPGA_SYSREG_CFGMEMCTL (0x4F000010)
+#define SCOBCA1_FPGA_HRMEM_ADDR       (0x60000000)
+#define SYSREG_CODEMSEL_ENABLE_HRMEM  (0x5a5a0000)
+#define SCOBCA1_FPGA_CFGBOOTMEM(x)    ((((x) & 0x00001000) >> 12))
+#define SCOBCA1_FPGA_CFGMEMMON(x)     ((((x) & 0x00000020) >> 5))
+#define SCOBCA1_FPGA_CFGMEMSEL(x)     ((((x) & 0x00000001) << 4))
+#define QSPI_RX_FIFO_MAX_BYTE         (16U)
+#define CFG_MEMSEL_RETRY_COUNT        (100U)
+#define CFG_MEMSEL_RETRY_INTERVAL     K_MSEC(1)
+
+static int select_cfg_memory(void)
+{
+	int ret = -1;
+	uint8_t boot_bank;
+	uint8_t sel_bank;
+	mm_reg_t sysreg = SCOBCA1_FPGA_SYSREG_CFGMEMCTL;
+	uint16_t retry = CFG_MEMSEL_RETRY_COUNT;
+
+	/*
+	 * FPGA configures using the Config memory bank specified by TRCH,
+	 * but after control is transferred to the FPGA, the Config Memory
+	 * selects the default 0 bank. Therefore, the Bootloader needs to
+	 * check the FPGA registers to determine which bank was used for
+	 * configuration, and then load the FSW from the same bank and copy
+	 * it to HRMEM.
+	 */
+	boot_bank = SCOBCA1_FPGA_CFGBOOTMEM(sys_read32(sysreg));
+
+	sys_write32(SCOBCA1_FPGA_CFGMEMSEL(boot_bank), sysreg);
+
+	while (retry--) {
+		sel_bank = SCOBCA1_FPGA_CFGMEMMON(sys_read32(sysreg));
+		if (sel_bank == boot_bank) {
+			printk("Boot from CFG bank %d\n", sel_bank);
+			ret = 0;
+			break;
+		}
+
+		k_sleep(CFG_MEMSEL_RETRY_INTERVAL);
+	}
+
+	return ret;
+}
 
 static int copy_norflash_to_hrmem(uint8_t partition_id, mm_reg_t sram_mem_addr, uint32_t size)
 {
@@ -21,6 +61,11 @@ static int copy_norflash_to_hrmem(uint8_t partition_id, mm_reg_t sram_mem_addr, 
 	uint8_t read_vals[QSPI_RX_FIFO_MAX_BYTE];
 	uint16_t loop_count;
 	off_t offset = 0;
+
+	ret = select_cfg_memory();
+	if (ret < 0) {
+		goto end;
+	}
 
 	ret = flash_area_open(partition_id, &flash);
 	if (ret < 0) {
