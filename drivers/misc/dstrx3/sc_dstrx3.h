bb33f8ad8bfd87356d008831e0b4a326598042a3
Takuya Sasaki <takuya.sasaki@spacecubics.com>
2024-09-02 20:38:55 +0900

    drivers/misc/dstrx3: Add set default tx param API
    This commit adds an API to set the default TX parameters configured
    in the Zephyr DTS.
    
    Signed-off-by: Takuya Sasaki <takuya.sasaki@spacecubics.com>

--- drivers/misc/dstrx3/sc_dstrx3.h
+++ drivers/misc/dstrx3/sc_dstrx3.h
@@ -58,3 +58,4 @@ void sc_dstrx3_disable_cmdif(const struct device *dev);
 int sc_dstrx3_get_hk_telemetry(const struct device *dev, struct sc_dstrx3_hk *hk);
 void sc_dstrx3_set_tx_param(const struct device *dev, enum sc_dstrx3_tx_power tx_power,
 			    enum sc_dstrx3_bit_rate bit_rate);
+void sc_dstrx3_set_default_tx_param(const struct device *dev);
