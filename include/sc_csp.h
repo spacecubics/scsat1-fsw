67b84ed9c7da4656c87d6779a6f26c718bb45dfb
Takuya Sasaki <takuya.sasaki@spacecubics.com>
2024-10-09 19:26:40 +0900

    include: Add CSP related header
    This commit adds a common header related to the CSP for SC-Sat1. The
    CSP ID may be subject to change in the future.
    
    Signed-off-by: Takuya Sasaki <takuya.sasaki@spacecubics.com>

---
+++ include/sc_csp.h
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2024 Space Cubics, LLC.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#pragma once
+
+#define CSP_ID_MAIN_CAN1 (1U)
+#define CSP_ID_ADCS      (2U)
+#define CSP_ID_EPS       (4U)
+#define CSP_ID_SRS3      (5U)
+#define CSP_ID_GND       (10U)
+#define CSP_ID_MAIN_CAN2 (11U)
+#define CSP_ID_ZERO      (12U)
+#define CSP_ID_PICO      (14U)
+
+#define CSP_CAN_BITRATE (1000000U)
