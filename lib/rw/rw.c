1b081e2a7cf7405fdf92fc23527ca19f41597899
Takuya Sasaki <takuya.sasaki@spacecubics.com>
2024-10-30 19:33:50 +0900

    lib/rw: Move Reaction Wheel API from hwtest
    This commit moves APIs related to Reaction Wheel from test/hwtest to
    lib. This library is only enabled for the ADCS Board.
    
    Signed-off-by: Takuya Sasaki <takuya.sasaki@spacecubics.com>

--- lib/rw/rw.c
+++ lib/rw/rw.c
@@ -9,7 +9,7 @@
 #include "cv_adcs.h"
 
 #include <zephyr/logging/log.h>
-LOG_MODULE_REGISTER(rw, CONFIG_SCSAT1_ADCS_LOG_LEVEL);
+LOG_MODULE_REGISTER(rw, CONFIG_SC_LIB_RW_LOG_LEVEL);
 
 #define SC_RWM_BASE_ADDR    (0x50030000)
 #define SC_RWM_MENR_OFFSET  (0x0000)
