0c016980be6b1e834cf3424639f0edb074c2a4be
Takuya Sasaki <takuya.sasaki@spacecubics.com>
2024-11-01 00:24:50 +0900

    drivers/misc/fpga: Add ECC Error Collect API
    This commit adds an API to enable or disable ECC Error Collect. When
    this feature is enabled, the HRMEM IP core performs a write-back of
    the corrected data to repair the memory upon detection of a 1-bit ECC
    error. This feature is enabled by default in the HRMEM IP core.
    
    Signed-off-by: Takuya Sasaki <takuya.sasaki@spacecubics.com>

---
+++ drivers/misc/fpga/sc_fpgahrmem.h
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) 2024 Space Cubics, LLC.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#pragma once
+
+void sc_hrmem_enable_ecc_collect(void);
+void sc_hrmem_disable_ecc_collect(void);
