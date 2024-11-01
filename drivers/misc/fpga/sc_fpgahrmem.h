f5b8a638b19161075981a32de9fb9164d5f1c649
Takuya Sasaki <takuya.sasaki@spacecubics.com>
2024-11-01 10:18:16 +0900

    drivers/misc/fpga: Add 1Bit ECC Error Count API
    This commit adds an API to retrieve the 1-bit ECC error count. The
    1-bit ECC error count can be retrieved separately for counters
    detected by read from memory scrub or AHB/AXI bus.
    
    Signed-off-by: Takuya Sasaki <takuya.sasaki@spacecubics.com>

--- drivers/misc/fpga/sc_fpgahrmem.h
+++ drivers/misc/fpga/sc_fpgahrmem.h
@@ -10,3 +10,4 @@ void sc_hrmem_enable_ecc_collect(void);
 void sc_hrmem_disable_ecc_collect(void);
 void sc_hrmem_enable_memory_scrub(void);
 void sc_hrmem_disable_memory_scrub(void);
+int sc_hrmem_get_ecc_error_count(uint16_t *by_auto, uint16_t *by_bus);
