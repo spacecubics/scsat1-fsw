c2ccebb36a0ae68fdc2c495f46e207b81e758c45
Takuya Sasaki <takuya.sasaki@spacecubics.com>
2024-11-03 10:09:05 +0900

    lib/csp: Add time sync API from EPS
    This commit uses the `Get Time` command from EPS to obtain a
    timestamp and set it as the local time. The time set will be offset
    by the CSP communication latency, but this API does not apply any
    correction.
    
    Signed-off-by: Takuya Sasaki <takuya.sasaki@spacecubics.com>

---
+++ lib/csp/eps.c
@@ -0,0 +1,77 @@
+/*
+ * Copyright (c) 2024 Space Cubics, LLC.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/posix/time.h>
+#include <zephyr/sys/byteorder.h>
+#include <csp/csp.h>
+#include "sc_csp.h"
+
+#include <zephyr/logging/log.h>
+LOG_MODULE_REGISTER(eps, CONFIG_SC_LIB_CSP_LOG_LEVEL);
+
+#define CSP_PORT_EPS_GET_TIME (8U)
+#define CSP_EPS_GET_TIME_SIZE (5U)
+#define CSP_EPS_TIME_OFFSET   (1U)
+
+int csp_time_sync_from_eps(void)
+{
+	int ret = 0;
+	csp_conn_t *conn;
+	csp_packet_t *packet;
+	uint8_t command_id = 0;
+	struct timespec ts;
+
+	conn = csp_connect(CSP_PRIO_NORM, CSP_ID_EPS, CSP_PORT_EPS_GET_TIME,
+			   CONFIG_SC_LIB_CSP_CONN_TIMEOUT_MSEC, CSP_O_NONE);
+	if (conn == NULL) {
+		LOG_ERR("Failed to connect to EPS");
+		ret = -ETIME;
+		goto end;
+	}
+
+	packet = csp_buffer_get(0);
+	if (packet == NULL) {
+		LOG_ERR("Failed to get CSP buffer");
+		ret = -ENOMEM;
+		goto close;
+	}
+
+	memcpy(&packet->data, &command_id, sizeof(command_id));
+	packet->length = sizeof(command_id);
+
+	csp_send(conn, packet);
+	LOG_DBG("Send GET TIME packet to EPS");
+
+	packet = csp_read(conn, CONFIG_SC_LIB_CSP_READ_TIMEOUT_MSEC);
+	if (packet == NULL) {
+		LOG_ERR("Failed to read EPS GET TIME reply");
+		ret = -ETIME;
+		goto close;
+	}
+
+	if (packet->length != CSP_EPS_GET_TIME_SIZE) {
+		LOG_ERR("Invalid data size of EPS GET TIME reply (%d)", packet->length);
+		ret = -EINVAL;
+		goto close;
+	}
+
+	ts.tv_sec = sys_le32_to_cpu(*(uint32_t *)&packet->data[CSP_EPS_TIME_OFFSET]);
+	ts.tv_nsec = 0;
+
+	ret = clock_settime(CLOCK_REALTIME, &ts);
+	if (ret < 0) {
+		LOG_ERR("Failed to set time (%d)", ret);
+		goto close;
+	}
+
+	LOG_INF("Time sync from EPS (%lld)", ts.tv_sec);
+
+close:
+	csp_close(conn);
+
+end:
+	return ret;
+}
