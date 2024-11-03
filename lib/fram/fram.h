f6b76326cf0909c77144c0ac6c2171806a0eb436
Takuya Sasaki <takuya.sasaki@spacecubics.com>
2024-11-03 19:42:55 +0900

    lib/fram: Add FRAM read/write API
    This commit adds APIs for reading from and writing to FRAM. However,
    these APIs support only up to 16 bytes for each read and write
    operation at a time. Additionally, the implementation of this library
    does not use the SPI driver but instead directly operates the
    registers of the FPGA QSPI core. This code is based on a simplified
    version previously written for testing purposes.
    
    Signed-off-by: Takuya Sasaki <takuya.sasaki@spacecubics.com>

---
+++ lib/fram/fram.h
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2022 Space Cubics, LLC.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#pragma once
+
+#define SC_FRAM_MEM0 (0U)
+#define SC_FRAM_MEM1 (1U)
+
+int sc_fram_write(uint8_t mem_no, uint32_t mem_addr, uint32_t size, uint8_t *val);
+int sc_fram_read(uint8_t mem_no, uint32_t mem_addr, uint32_t size, uint8_t *val);
