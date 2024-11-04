5971f7adb9eba88e27edd84f0f2931bc7fff8fe4
Takuya Sasaki <takuya.sasaki@spacecubics.com>
2024-11-04 14:10:38 +0900

    main,adcs: Add monitor work queue
    This commit adds a work queue for monitoring sensor data and other
    parameters onboard SC-Sat1. The work queue is designed to be used
    commonly across various sensors, with configurable stack size and
    priority settings available in Kconfig.
    
    Signed-off-by: Takuya Sasaki <takuya.sasaki@spacecubics.com>

---
+++ adcs/src/mon/monitor.c
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2024 Space Cubics, LLC.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/kernel.h>
+
+#include <zephyr/logging/log.h>
+LOG_MODULE_REGISTER(monitor, CONFIG_SCSAT1_ADCS_LOG_LEVEL);
+
+K_THREAD_STACK_DEFINE(monitor_workq_stack, CONFIG_SCSAT1_ADCS_MONITOR_THREAD_STACK_SIZE);
+struct k_work_q monitor_workq;
+
+void start_monitor(void)
+{
+	k_work_queue_start(&monitor_workq, monitor_workq_stack,
+			   K_THREAD_STACK_SIZEOF(monitor_workq_stack),
+			   CONFIG_SCSAT1_ADCS_MONITOR_THREAD_PRIORITY, NULL);
+	k_thread_name_set(&monitor_workq.thread, "monitor_workq");
+}
