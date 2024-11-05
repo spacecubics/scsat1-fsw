d91d7248fdfec1bbc80e04a279e8eba604a8cd5d
Takuya Sasaki <takuya.sasaki@spacecubics.com>
2024-11-05 13:22:03 +0900

    include: Change CSP ID
    This commit changes the CSP ID of the Pico to align with actual
    operations.
    
    Signed-off-by: Takuya Sasaki <takuya.sasaki@spacecubics.com>

--- include/sc_csp.h
+++ include/sc_csp.h
@@ -6,14 +6,14 @@
 
 #pragma once
 
-#define CSP_ID_MAIN_CAN1 (1U)
-#define CSP_ID_ADCS      (2U)
+#define CSP_ID_GND       (1U)
+#define CSP_ID_SRS3      (2U)
 #define CSP_ID_EPS       (4U)
-#define CSP_ID_SRS3      (5U)
-#define CSP_ID_GND       (10U)
-#define CSP_ID_MAIN_CAN2 (11U)
-#define CSP_ID_ZERO      (12U)
-#define CSP_ID_PICO      (14U)
+#define CSP_ID_MAIN_CAN1 (8U)
+#define CSP_ID_MAIN_CAN2 (9U)
+#define CSP_ID_ADCS      (16U)
+#define CSP_ID_ZERO      (24U)
+#define CSP_ID_PICO      (26U)
 
 #define CSP_TLM_ID_SYSHK (0U)
 #define CSP_PORT_TLM     (10U)
