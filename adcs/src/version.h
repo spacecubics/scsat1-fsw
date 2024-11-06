e8f0954c44c2a69b5f2e54bc9979a5773c5f70b4
Takuya Sasaki <takuya.sasaki@spacecubics.com>
2024-11-06 09:32:05 +0900

    main,adcs: Add version information
    This commit adds the version information for flight software.
    
    Signed-off-by: Takuya Sasaki <takuya.sasaki@spacecubics.com>

---
+++ adcs/src/version.h
@@ -0,0 +1,9 @@
+/*
+ * Copyright (c) 2024 Space Cubics, LLC.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#pragma once
+
+#define ADCS_FSW_VERSION "0.1.0"
---
+++ main/src/version.h
@@ -0,0 +1,9 @@
+/*
+ * Copyright (c) 2024 Space Cubics, LLC.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#pragma once
+
+#define MAIN_FSW_VERSION "0.1.0"
