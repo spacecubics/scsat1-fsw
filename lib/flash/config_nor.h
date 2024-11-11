e9cc5722b3ecc37f0f976f03eb7bc7d64fa55760
Takuya Sasaki <takuya.sasaki@spacecubics.com>
2024-11-11 21:53:06 +0900

    lib/flash: Add calculate CRC32 API
    This commit adds an API to calculate the CRC32 of the Configuration
    NOR flash. Although offset and size can be specified, if both are
    set to 0, the entire specified partition will be calculated.
    
    Signed-off-by: Takuya Sasaki <takuya.sasaki@spacecubics.com>

--- lib/flash/config_nor.h
+++ lib/flash/config_nor.h
@@ -9,3 +9,4 @@
 #include <stdint.h>
 
 int sc_config_nor_erase(uint8_t bank, uint8_t id, off_t offset, size_t size);
+int sc_config_nor_calc_crc(uint8_t bank, uint8_t id, off_t offset, size_t size, uint32_t *crc32);
