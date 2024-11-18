33fefa80f4c57f095d759103604a1a09a1f8dbc9
Takuya Sasaki <takuya.sasaki@spacecubics.com>
2024-11-19 08:39:51 +0900

    adcs: Add IMU enable/disable command
    Adds the enable or disable command handler for IMU.
    
    Signed-off-by: Takuya Sasaki <takuya.sasaki@spacecubics.com>

--- adcs/src/cmd/pwrctrl.c
+++ adcs/src/cmd/pwrctrl.c
@@ -8,6 +8,7 @@
 #include "sc_csp.h"
 #include "reply.h"
 #include "pwrctrl_adcs.h"
+#include "imu.h"
 
 #include <zephyr/logging/log.h>
 LOG_MODULE_REGISTER(pwrctrl, CONFIG_SCSAT1_ADCS_LOG_LEVEL);
@@ -16,14 +17,17 @@ LOG_MODULE_REGISTER(pwrctrl, CONFIG_SCSAT1_ADCS_LOG_LEVEL);
 #define PWRCTRL_CMD_MIN_SIZE   (1U)
 #define PWRCTRL_ONOFF_CMD_SIZE (3U)
 #define PWRCTRL_CYCLE_CMD_SIZE (1U)
+#define PWRCTRL_IMU_CMD_SIZE   (2U)
 
 /* Command ID */
 #define PWRCTRL_ONOFF_CMD (0U)
 #define PWRCTRL_CYCLE_CMD (1U)
+#define PWRCTRL_IMU_CMD   (2U)
 
 /* Command argument offset */
-#define PWRCTRL_TARGET_OFFSET (1U)
-#define PWRCTRL_ONOFF_OFFSET  (2U)
+#define PWRCTRL_TARGET_OFFSET    (1U)
+#define PWRCTRL_ONOFF_OFFSET     (2U)
+#define PWRCTRL_IMU_ONOFF_OFFSET (1U)
 
 static int csp_power_control_onoff_cmd(uint8_t command_id, csp_packet_t *packet)
 {
@@ -70,6 +74,32 @@ end:
 	return ret;
 }
 
+static int csp_imu_enable_cmd(uint8_t command_id, csp_packet_t *packet)
+{
+	int ret = 0;
+	uint8_t onoff;
+
+	if (packet->length != PWRCTRL_IMU_CMD_SIZE) {
+		LOG_ERR("Invalide command size: %d", packet->length);
+		ret = -EINVAL;
+		goto end;
+	}
+
+	onoff = packet->data[PWRCTRL_IMU_ONOFF_OFFSET];
+
+	LOG_INF("IMU enable command (onoff: %d)", onoff);
+
+	if (onoff) {
+		imu_enable();
+	} else {
+		imu_disable();
+	}
+
+end:
+	csp_send_std_reply(packet, command_id, ret);
+	return ret;
+}
+
 int csp_pwrctrl_handler(csp_packet_t *packet)
 {
 	int ret = 0;
@@ -95,6 +125,9 @@ int csp_pwrctrl_handler(csp_packet_t *packet)
 		break;
 	case PWRCTRL_CYCLE_CMD:
 		csp_power_cycle_cmd(command_id, packet);
+		break;
+	case PWRCTRL_IMU_CMD:
+		csp_imu_enable_cmd(command_id, packet);
 		break;
 	default:
 		LOG_ERR("Unkown command code: %d", command_id);
