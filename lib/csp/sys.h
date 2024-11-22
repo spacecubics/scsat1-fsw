3d99ded5af88a8839a895ba160d7ad6135feb573
Takuya Sasaki <takuya.sasaki@spacecubics.com>
2024-11-22 14:10:39 +0900

    lib/csp: Add Read FPGA register command
    This commit adds a command to read the address of an arbitrary FPGA
    register and return the value to the ground.
    
    Signed-off-by: Takuya Sasaki <takuya.sasaki@spacecubics.com>

--- lib/csp/sys.h
+++ lib/csp/sys.h
@@ -14,5 +14,11 @@ struct csp_stat {
 	uint8_t last_command_id;
 };
 
+struct system_reg_telemetry {
+	uint8_t telemetry_id;
+	uint32_t error_code;
+	uint32_t reg_value;
+} __attribute__((__packed__));
+
 int csp_system_handler(csp_packet_t *packet);
 void csp_system_update_stat(csp_packet_t *packet, struct csp_stat *stat);
